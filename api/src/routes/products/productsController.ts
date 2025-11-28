f903574d2e84d17ea49195dae08e22f97e59d769
SALIOU Baninou <salioubaninou@gmail.com>
2025-11-28 13:29:15 +0100

    Creation du crud prodducts

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,22 +1,59 @@
 import { Request, Response } from "express";
-export function productList(req: Request, res: Response){
-    return res.send("List of products")
+import { db } from "../../db/index";
+import { productsTable } from "../../db/productsSchema";
+import { eq } from "drizzle-orm";
+export async function productList(req: Request, res: Response){
+    try{
+        const products = await db.select().from(productsTable)
+        if(products.length <=0) return res.status(404).json({message: "Products not found !"})
+        return res.status(200).json(products)
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err)
+    }
 }
 
-export function getProductById(req: Request, res: Response){
-    console.log(req.params)
-    return res.send("Product id")
+export async function getProductById(req: Request, res: Response){
+    try{
+        const product = await db.select().from(productsTable).where(eq(productsTable.id, Number(req.params.id)))
+        if(product.length <=0) return res.status(404).json({message: "Product not found !"})
+        return res.status(200).json(product)
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err)
+    }
 }
 
-export function createProduct(req: Request, res: Response){
-    console.log(req.body)
-    return res.send("Create product")
+export async function createProduct(req: Request, res: Response){
+    try{
+        const [product] = await db.insert(productsTable).values(req.body).returning()
+        return res.status(201).json(product)
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err)
+    }
 }
 
-export function updateProduct(req: Request, res: Response){
-    return res.send("Update product")
+export async function updateProduct(req: Request, res: Response){
+    try{
+        const {id} = req.params
+        const [product] = await db.update(productsTable).set(req.body).where(eq(productsTable.id, Number(id))).returning()
+        if(!product) return res.status(404).json({message: "Product not found !"})
+        return res.status(201).json(product)
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err)
+    }
 }
 
-export function deleteProduct(req: Request, res: Response){
-    return res.send("Delete product")
+export async function deleteProduct(req: Request, res: Response){
+    try{
+        const { id } = req.params
+        const [product] = await db.delete(productsTable).where(eq(productsTable.id, Number(id))).returning()
+        if(!product) return res.status(404).json({message: "Product not found !"})
+        return res.status(204).json({message: "Product deleted successful !"})
+    }catch(err){
+        console.log(err)
+        return res.status(500).send(err)
+    }
 }
